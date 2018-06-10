496fff4cd79bf1fe12352e094c89e4eb8e4c5900
Hai Bui <buixuanhai.it@gmail.com>
2018-06-10 15:15:07 +0700

    Update eslint

--- src/components/ParkingLotMap.js
+++ src/components/ParkingLotMap.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { compose, withProps } from 'recompose';
+import React from "react";
+import { compose, withProps } from "recompose";
 import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from 'react-google-maps';
+	withScriptjs,
+	withGoogleMap,
+	GoogleMap,
+	Marker
+} from "react-google-maps";
 
 const ParkingLotMap = compose(
-  withProps({
-    googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `100%` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap
+	withProps({
+		googleMapURL:
+			"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+		loadingElement: <div style={{ height: "100%" }} />,
+		containerElement: <div style={{ height: "100%" }} />,
+		mapElement: <div style={{ height: "100%" }} />
+	}),
+	withScriptjs,
+	withGoogleMap
 )(props => (
-  <GoogleMap
-    defaultZoom={10}
-    center={props.center || { lat: 10.470702, lng: 106.7588609 }}
-  >
-    {props.isMarkerShown && (
-      <Marker position={props.center} onClick={props.onMarkerClick} />
-    )}
-  </GoogleMap>
+	<GoogleMap
+		defaultZoom={10}
+		center={props.center || { lat: 10.470702, lng: 106.7588609 }}
+	>
+		{props.isMarkerShown && (
+			<Marker position={props.center} onClick={props.onMarkerClick} />
+		)}
+	</GoogleMap>
 ));
-ParkingLotMap.displayName = 'ParkingLotMap';
+ParkingLotMap.displayName = "ParkingLotMap";
 export default ParkingLotMap;
