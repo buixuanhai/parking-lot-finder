18623fcedb4b536ebf427a2c20acb0cd7ae413d0
Hai Bui <buixuanhai.it@gmail.com>
2018-06-24 14:21:28 +0700

    Fix unknown story id

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -9,58 +9,58 @@ import { compose } from "redux";
 import { Redirect } from "react-router-dom";
 import { push } from "react-router-redux";
 const Container = styled.div`
-	margin: 10px;
+  margin: 10px;
 `;
 
 const FormItem = Form.Item;
 
 class Login extends Component {
-	state = { login: null, password: null };
+  state = { login: null, password: null };
 
-	handleChange = field => ({ target: { value } }) => {
-	  this.setState({ [field]: value });
-	};
+  handleChange = field => ({ target: { value } }) => {
+    this.setState({ [field]: value });
+  };
 
-	handleCancel = () => {
-	  this.props.push("/");
-	};
+  handleCancel = () => {
+    this.props.push("/");
+  };
 
-	onSubmit = e => {
-	  e.preventDefault();
-	  this.props.firebase.login(this.state);
-	};
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.firebase.login(this.state);
+  };
 
-	render() {
-	  if (!this.props.auth.isEmpty) {
-	    return <Redirect to="/" />;
-	  }
-	  return (
-	    <Container>
-	      <Form layout="vertical" onSubmit={this.onSubmit}>
-	        <FormItem label="Email">
-	          <Input placeholder="Email" onChange={this.handleChange("email")} />
-	        </FormItem>
-	        <FormItem label="Password">
-	          <Input
-	            placeholder="Password"
-	            onChange={this.handleChange("password")}
-	            type="password"
-	          />
-	        </FormItem>
-	        <FormItem>
-	          <Button
-	            type="primary"
-	            htmlType="submit"
-	            style={{ marginRight: 20 }}
-	          >
-							Login
-	          </Button>
-	          <Button onClick={this.handleCancel}>Cancel</Button>
-	        </FormItem>
-	      </Form>
-	    </Container>
-	  );
-	}
+  render() {
+    if (!this.props.auth.isEmpty) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <Container>
+        <Form layout="vertical" onSubmit={this.onSubmit}>
+          <FormItem label="Email">
+            <Input placeholder="Email" onChange={this.handleChange("email")} />
+          </FormItem>
+          <FormItem label="Password">
+            <Input
+              placeholder="Password"
+              onChange={this.handleChange("password")}
+              type="password"
+            />
+          </FormItem>
+          <FormItem>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ marginRight: 20 }}
+            >
+              Login
+            </Button>
+            <Button onClick={this.handleCancel}>Cancel</Button>
+          </FormItem>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default compose(
--- src/screens/StoryDetail.js
+++ src/screens/StoryDetail.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import styled from "styled-components";
 import { compose } from "redux";
 import { firebaseConnect, populate } from "react-redux-firebase";
+import { Redirect } from "react-router-dom";
 import Button from "antd/lib/button";
 import { push } from "react-router-redux";
 
@@ -43,21 +44,25 @@ class StoryDetail extends Component {
   };
   render() {
     const { title, content, imageURL } = this.props;
-    return (
-      <Container>
-        {imageURL && <Image src={imageURL} />}
-        <Button
-          shape="circle"
-          icon="arrow-left"
-          style={{ position: "absolute", top: 10, left: 10, zIndex: 2 }}
-          onClick={this.handleBack}
-        />
-        <Header>
-          <Title>{title}</Title>
-        </Header>
-        <Content>{content}</Content>
-      </Container>
-    );
+    if (this.props.id) {
+      return (
+        <Container>
+          {imageURL && <Image src={imageURL} />}
+          <Button
+            shape="circle"
+            icon="arrow-left"
+            style={{ position: "absolute", top: 10, left: 10, zIndex: 2 }}
+            onClick={this.handleBack}
+          />
+          <Header>
+            <Title>{title}</Title>
+          </Header>
+          <Content>{content}</Content>
+        </Container>
+      );
+    } else {
+      return <Redirect to="/" />;
+    }
   }
 }
 
@@ -76,12 +81,16 @@ export default compose(
       if (stories) {
         const id = ownProps.match.params.id;
         const story = stories[id];
-        return {
-          id,
-          ...story,
-          imageURL: story.imageId ? story.imageId.downloadURL : null,
-          ...dispatchProps
-        };
+        if (story) {
+          return {
+            id,
+            ...story,
+            imageURL: story.imageId ? story.imageId.downloadURL : null,
+            ...dispatchProps
+          };
+        } else {
+          return { ...dispatchProps };
+        }
       } else {
         return { ...dispatchProps };
       }
